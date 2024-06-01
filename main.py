a5eb4c534aabdc37ad93e6ffaeee2e063d053fb7
Nicolas Restrepo Carvajal <105328047+niconomist98@users.noreply.github.com>
2024-06-01 23:00:15 +0000

    add option to run etl on validation dataset

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from pipeline import replace_missing_values,execute,insert_csv_line_sqlite,remove_temp_files
 import time
 if __name__ == "__main__":
-    raw_data_path = "Data/raw"
-    temp_path="Data/temp"
+    while True:
+        try:
+            option = int(input("Select a pipeline run option:\n1. Type (1) Run Pipeline on all the datasets\n2. Type (2) Run the pipeline on validation datasets only\n"))
+            if option in [1, 2]:
+                print(f"You selected option {option}.")
+                break
+            else:
+                print("Invalid option. Please type 1 or 2.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+    if option==1:
+        raw_data_path = "Data/raw"
+        temp_path="Data/temp"
+    elif option==2:
+        raw_data_path = "Data/validation"
+        temp_path="Data/temp"
+        
     replace_missing_values(raw_data_path,temp_path)
     sqlite_db_path='database/pragma.db'
     table_name='prices'
